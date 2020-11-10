2ce0eae3cbf8e516280871015f8a20dfc4050a6c
Mateusz Wołek <volkosky@gmail.com>
2020-11-10 22:32:37 +0100

    refactor

--- pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/OwnerService.java
@@ -2,11 +2,6 @@ package com.volkosky.sfgpetclinic.services;
 
 import com.volkosky.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
-    Owner findByID(Long id);
-    Owner save (Owner owner);
-    Set<Owner> findAll();
 }
