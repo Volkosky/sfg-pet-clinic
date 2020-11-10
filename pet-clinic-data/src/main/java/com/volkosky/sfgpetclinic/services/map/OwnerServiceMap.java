b6fd870d0ed8c0c9093d743f5173fb11a90a2c38
Mateusz Wołek <volkosky@gmail.com>
2020-11-10 23:45:42 +0100

    map services

---
+++ pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.volkosky.sfgpetclinic.services.map;
+
+import com.volkosky.sfgpetclinic.model.Owner;
+import com.volkosky.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner,Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findByID(Long id) {
+        return super.findByID(id);
+    }
+}
