da79664a0d45879f589785032427fa20da0d5067
Mateusz Wołek <volkosky@gmail.com>
2020-11-22 22:30:10 +0100

    PetTypeService added

---
+++ pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.volkosky.sfgpetclinic.services;
+
+import com.volkosky.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package com.volkosky.sfgpetclinic.services.map;
+
+import com.volkosky.sfgpetclinic.model.PetType;
+import com.volkosky.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService{
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findByID(Long id) {
+        return super.findByID(id);
+    }
+}
