7745fda6b966af4e69eae3a7f14be1094dffe5f2
Mateusz Wołek <volkosky@gmail.com>
2020-12-01 23:22:17 +0100

    visit map service

---
+++ pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.volkosky.sfgpetclinic.services;
+
+import com.volkosky.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.volkosky.sfgpetclinic.services.map;
+
+import com.volkosky.sfgpetclinic.model.Visit;
+import com.volkosky.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null ||
+           visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findByID(Long id) {
+        return super.findByID(id);
+    }
+}
