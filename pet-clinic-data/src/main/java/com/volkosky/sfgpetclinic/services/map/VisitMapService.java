b156cbd44c88dfde9a518a4a3c86d37ae7d9da58
Mateusz Wołek <volkosky@gmail.com>
2020-12-01 23:22:36 +0100

    visit map service

--- pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/volkosky/sfgpetclinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package com.volkosky.sfgpetclinic.services.map;
 
 import com.volkosky.sfgpetclinic.model.Visit;
 import com.volkosky.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
